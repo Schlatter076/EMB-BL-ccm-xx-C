05a5427395644d7c9e77e7f7cfd5e661d2e4cb1c
TLY <taoliyong@tacsense.cn>
2021-05-31 18:47:10 +0800

    feat: driver leds by adc value

--- Apps/main/src/main.BL.c
+++ Apps/main/src/main.BL.c
@@ -27,10 +27,7 @@
 #include "../../callbacks/inc/tsTracerRxCallBacks.h"
 
 static const ksU08 lc_cmdGetForce[] =
-{
-    0x01, 0x03, 0x00, 0x00,
-    0x00, 0x0D, 0x84, 0x0F,
-};
+{ 0x01, 0x03, 0x00, 0x00, 0x00, 0x0D, 0x84, 0x0F, };
 
 static float lv_forceInGF = 0.1f;
 // 18 -> PC; 14 -> AiDeBao; 05 <- WM
@@ -59,13 +56,18 @@ static float lv_forceInGF = 0.1f;
 #define   __WANMO_PKT_LEN       8 // */ 9
 #endif
 
-static ksU32    lv_aiDeBaoCntr      = 0x00;
-static ksU32    lv_wmUartCntr       = 0x00;
+static ksU32 lv_aiDeBaoCntr = 0x00;
+static ksU32 lv_wmUartCntr = 0x00;
 
-static ksS32    lv_filteredADC      = 0x00;
-static ksS32    lv_Vout             = 0x00;
-static ksS32    lv_resistorVal      = 0x00;
-static ksS32    lv_resistorValPrev  = 0x00;
+static ksS32 lv_filteredADC = 0x00;
+static ksS32 lv_Vout = 0x00;
+static ksS32 lv_resistorVal = 0x00;
+static ksS32 lv_resistorValPrev = 0x00;
+
+static ksS32 lv_bl_filteredADC = 0x00;
+static ksS32 lv_bl_Vout = 0x00;
+static ksS32 lv_bl_resistorVal = 0x00;
+static ksS32 lv_bl_resistorValPrev = 0x00;
 
 #if (__THOR_MODE == _THOR_WANMO)
 /**
@@ -155,108 +157,106 @@ __EXIT_wmUartReturnRecvISR :
 }
 #endif
 
-
 /**
  * @fn aiDeBaoRcvdDataHandler
  */
-static void aiDeBaoRcvdDataHandler (ksTracer_t *pTracer, ksU08 *pByte)
+static void aiDeBaoRcvdDataHandler(ksTracer_t *pTracer, ksU08 *pByte)
 {
-  uchar        rcvdByte  = *pByte;
-  tracerBuf_t  *pRxBuf   = &uTracer[__UTRACER_IDX_1].rxBuf;
+	uchar rcvdByte = *pByte;
+	tracerBuf_t *pRxBuf = &uTracer[__UTRACER_IDX_1].rxBuf;
 
-  pRxBuf->buf[pRxBuf->len++] = rcvdByte;
+	pRxBuf->buf[pRxBuf->len++] = rcvdByte;
 
-  if (lv_aiDeBaoCntr == __AIDEBAO_PKT_LEN)
-  {
-    pRxBuf->status = _tracer_Recved;
-  }
-  else if (lv_aiDeBaoCntr > __AIDEBAO_PKT_LEN)
-  {
-    lv_aiDeBaoCntr = 0x00;
-    deInitUtracerRxBuf();
-  }
+	if (lv_aiDeBaoCntr == __AIDEBAO_PKT_LEN)
+	{
+		pRxBuf->status = _tracer_Recved;
+	}
+	else if (lv_aiDeBaoCntr > __AIDEBAO_PKT_LEN)
+	{
+		lv_aiDeBaoCntr = 0x00;
+		deInitUtracerRxBuf();
+	}
 }
 
 /**
  * @fn aiDeBaoReturnRecvISR
  */
-static void aiDeBaoReturnRecvISR (void)
+static void aiDeBaoReturnRecvISR(void)
 {
-  ksUSART_t *pUart = uTracer[__UTRACER_IDX_1].pUsart;
+	ksUSART_t *pUart = uTracer[__UTRACER_IDX_1].pUsart;
 
-  if(__USART_RECV_is_PENDING(pUart))
-  {
-    ksU08           rcvdByte  = ksUsartGetByte(pUart);
-    tracerStatus_e  status    = uTracer[__UTRACER_IDX_1].rxBuf.status;
+	if (__USART_RECV_is_PENDING(pUart))
+	{
+		ksU08 rcvdByte = ksUsartGetByte(pUart);
+		tracerStatus_e status = uTracer[__UTRACER_IDX_1].rxBuf.status;
 
-    switch (status)
-    {
-      case _tracer_free :
-      { // the 1st byte
-        switch (lv_aiDeBaoCntr)
-        {
-          case 0x00 :
-          {
-            if (0x01 != rcvdByte)
-            {
-              lv_aiDeBaoCntr = 0x00;
-              deInitUtracerRxBuf();
-              goto __EXIT_aiDeBaoReturnRecvISR;
-            }
-          }
-          break;
+		switch (status)
+		{
+		case _tracer_free:
+		{ // the 1st byte
+			switch (lv_aiDeBaoCntr)
+			{
+			case 0x00:
+			{
+				if (0x01 != rcvdByte)
+				{
+					lv_aiDeBaoCntr = 0x00;
+					deInitUtracerRxBuf();
+					goto __EXIT_aiDeBaoReturnRecvISR;
+				}
+			}
+				break;
 
-          case 0x01 :
-          {
-            if (0x03 != rcvdByte)
-            {
-              lv_aiDeBaoCntr = 0x00;
-              deInitUtracerRxBuf();
-              goto __EXIT_aiDeBaoReturnRecvISR;
-            }
-          }
-          break;
+			case 0x01:
+			{
+				if (0x03 != rcvdByte)
+				{
+					lv_aiDeBaoCntr = 0x00;
+					deInitUtracerRxBuf();
+					goto __EXIT_aiDeBaoReturnRecvISR;
+				}
+			}
+				break;
 
-          case 0x02 :
-          {
-            if (0x1a == rcvdByte)
-            {
-              uTracer[__UTRACER_IDX_1].rxBuf.status  = _tracer_Locked;
-            }
-            else
-            {
-              lv_aiDeBaoCntr = 0x00;
-              deInitUtracerRxBuf();
-              goto __EXIT_aiDeBaoReturnRecvISR;
-            }
-          }
-          break;
+			case 0x02:
+			{
+				if (0x1a == rcvdByte)
+				{
+					uTracer[__UTRACER_IDX_1].rxBuf.status = _tracer_Locked;
+				}
+				else
+				{
+					lv_aiDeBaoCntr = 0x00;
+					deInitUtracerRxBuf();
+					goto __EXIT_aiDeBaoReturnRecvISR;
+				}
+			}
+				break;
 
-          default :
-            break;
-        }
-      }
-      break;
+			default:
+				break;
+			}
+		}
+			break;
 
+		case _tracer_Locked:
+		{
+			asm("nop");
+		}
+			break;
 
-      case _tracer_Locked :
-      {
-        asm("nop");
-      }
-      break;
+		default:
+			goto __EXIT_aiDeBaoReturnRecvISR;
+		}
 
-      default :
-        goto __EXIT_aiDeBaoReturnRecvISR;
-    }
+		++lv_aiDeBaoCntr;
+		aiDeBaoRcvdDataHandler(&uTracer[__UTRACER_IDX_1], &rcvdByte);
 
-    ++lv_aiDeBaoCntr;
-    aiDeBaoRcvdDataHandler(&uTracer[__UTRACER_IDX_1], &rcvdByte);
+		__EXIT_aiDeBaoReturnRecvISR: ksUsartClrITFlag(pUart,
+				pUart->RX_isr.flag);
 
-__EXIT_aiDeBaoReturnRecvISR :
-    ksUsartClrITFlag(pUart, pUart->RX_isr.flag);
-
-    NRF_UART0->EVENTS_ERROR = 0x00;
-  }
+		NRF_UART0->EVENTS_ERROR = 0x00;
+	}
 }
 
 /**
@@ -265,27 +265,27 @@ __EXIT_aiDeBaoReturnRecvISR :
  */
 static void uTracerRemapUart(ksU32 pin)
 {
-  switch (pin)
-  {
-    case __PIN_TO_ADB :
-    {
-      NRF_UART0->PSELTXD  = pin;
-      NRF_UART0->PSELRXD  = __PIN_FROM_ADB;
-      NRF_UART0->BAUDRATE = 2576384; // 9600
+	switch (pin)
+	{
+	case __PIN_TO_ADB:
+	{
+		NRF_UART0->PSELTXD = pin;
+		NRF_UART0->PSELRXD = __PIN_FROM_ADB;
+		NRF_UART0->BAUDRATE = 2576384; // 9600
 
-      NRF52832_ISR_IntVectSet(UARTE0_UART0_IRQn, aiDeBaoReturnRecvISR);
-    }
-    break;
+		NRF52832_ISR_IntVectSet(UARTE0_UART0_IRQn, aiDeBaoReturnRecvISR);
+	}
+		break;
 
-    case __PIN_TO_PC :
-    {
-      NRF_UART0->PSELTXD  = pin;
-      NRF_UART0->PSELRXD  = __PIN_FROM_PC;
-      NRF_UART0->BAUDRATE = 2576384; // 9600
+	case __PIN_TO_PC:
+	{
+		NRF_UART0->PSELTXD = pin;
+		NRF_UART0->PSELRXD = __PIN_FROM_PC;
+		NRF_UART0->BAUDRATE = 2576384; // 9600
 
-      NRF52832_ISR_IntVectSet(UARTE0_UART0_IRQn, aiDeBaoReturnRecvISR);
-    }
-    break;
+		NRF52832_ISR_IntVectSet(UARTE0_UART0_IRQn, aiDeBaoReturnRecvISR);
+	}
+		break;
 
 #if (__THOR_MODE == _THOR_WANMO)
     case __PIN_FROM_WM :
@@ -298,34 +298,45 @@ static void uTracerRemapUart(ksU32 pin)
     break;
 #endif
 
-    default :
-      break;
-  }
+	default:
+		break;
+	}
 
-  ksDelayMsByLoop(10);
+	ksDelayMsByLoop(10);
 }
 
-ksU32   lv_THOR_KineState;
-ksU32   lv_THOR_qActiveHead;
-ksU32   lv_THOR_qActiveTail;
-ksU32   lv_THOR_qActiveUsed;
-ksU32   lv_THOR_qActiveBuf[0x05];
+ksU32 lv_THOR_KineState;
+ksU32 lv_BL_KineState;
+ksU32 lv_THOR_qActiveHead;
+ksU32 lv_BL_qActiveHead;
+ksU32 lv_THOR_qActiveTail;
+ksU32 lv_BL_qActiveTail;
+ksU32 lv_THOR_qActiveUsed;
+ksU32 lv_BL_qActiveUsed;
+ksU32 lv_THOR_qActiveBuf[0x05];
+ksU32 lv_BL_qActiveBuf[0x05];
 
 /**
  * @fn    thorDeInitKineStateQ
  * @brief De-Initialize the queue used to buffer delta baseline
  */
-static void thorDeInitKineStateQ (void)
+static void thorDeInitKineStateQ(void)
 {
-  lv_THOR_qActiveUsed  = 0x00;
-  lv_THOR_qActiveHead  = 0x00;
-  lv_THOR_qActiveTail  = 0x00;
-  lv_THOR_KineState    = 0x00;
+	lv_THOR_qActiveUsed = 0x00;
+	lv_THOR_qActiveHead = 0x00;
+	lv_THOR_qActiveTail = 0x00;
+	lv_THOR_KineState = 0x00;
 
-  for(int i=0;i<0x05;++i)
-  {
-    lv_THOR_qActiveBuf[i] = 0x00;
-  }
+	lv_BL_qActiveUsed = 0x00;
+	lv_BL_qActiveHead = 0x00;
+	lv_BL_qActiveTail = 0x00;
+	lv_BL_KineState = 0x00;
+
+	for (int i = 0; i < 0x05; ++i)
+	{
+		lv_THOR_qActiveBuf[i] = 0x00;
+		lv_BL_qActiveBuf[i] = 0x00;
+	}
 }
 
 /**
@@ -334,23 +345,23 @@ static void thorDeInitKineStateQ (void)
  * @param valB
  * @return
  */
-static ksU32 thorGetAbsVal (ksS32 valA, ksS32 valB)
+static ksU32 thorGetAbsVal(ksS32 valA, ksS32 valB)
 {
-  ksS32 delta = 0x00;
+	ksS32 delta = 0x00;
 
-  if (valA >= valB)
-  {
-    delta = valA - valB;
-  }
-  else
-  {
-    delta = valB - valA;
-  }
+	if (valA >= valB)
+	{
+		delta = valA - valB;
+	}
+	else
+	{
+		delta = valB - valA;
+	}
 
-  ksU32 absVal = 0x00;
-  absVal = delta;
+	ksU32 absVal = 0x00;
+	absVal = delta;
 
-  return absVal;
+	return absVal;
 }
 
 /**
@@ -359,13 +370,13 @@ static ksU32 thorGetAbsVal (ksS32 valA, ksS32 valB)
  * @brief   update the base value of the sensor's admittance by weighedMAFilter
  * @note    the algorithym is executed only when the sensor is in FREE state
  */
-static void thorBufferKinestate (void)
+static void thorBufferKinestate(void)
 {
-  ksU32 deltaAbs = 0x00;
-  deltaAbs = thorGetAbsVal(lv_resistorValPrev, lv_resistorVal);
+	ksU32 deltaAbs = 0x00;
+	deltaAbs = thorGetAbsVal(lv_resistorValPrev, lv_resistorVal);
 
 #if   (__ANTI_NOISE_STRATEGY >= __ANTI_NOISE_LEVEL_4)
-  deltaAbs >>= 0x03;
+	deltaAbs >>= 0x03;
 #elif (__ANTI_NOISE_STRATEGY >= __ANTI_NOISE_LEVEL_3)
   deltaAbs >>= 0x02;
 #elif (__ANTI_NOISE_STRATEGY >= __ANTI_NOISE_LEVEL_2)
@@ -378,20 +389,55 @@ static void thorBufferKinestate (void)
 
 #endif
 
-  if(0x05 == lv_THOR_qActiveUsed)
-  { // the Q is full
-    lv_THOR_KineState      -= lv_THOR_qActiveBuf[lv_THOR_qActiveHead];
-    ++lv_THOR_qActiveHead;
-    lv_THOR_qActiveHead    %= 0x05; // pop Data from Head
-    --lv_THOR_qActiveUsed;
-  }
+	if (0x05 == lv_THOR_qActiveUsed)
+	{ // the Q is full
+		lv_THOR_KineState -= lv_THOR_qActiveBuf[lv_THOR_qActiveHead];
+		++lv_THOR_qActiveHead;
+		lv_THOR_qActiveHead %= 0x05; // pop Data from Head
+		--lv_THOR_qActiveUsed;
+	}
 
-  lv_THOR_qActiveBuf[lv_THOR_qActiveTail]  =  deltaAbs;     // put Data at Tail
-  ++lv_THOR_qActiveTail;
-  lv_THOR_qActiveTail      %= 0x05;
-  ++lv_THOR_qActiveUsed;
+	lv_THOR_qActiveBuf[lv_THOR_qActiveTail] = deltaAbs;     // put Data at Tail
+	++lv_THOR_qActiveTail;
+	lv_THOR_qActiveTail %= 0x05;
+	++lv_THOR_qActiveUsed;
 
-  lv_THOR_KineState += deltaAbs; // less than 255
+	lv_THOR_KineState += deltaAbs; // less than 255
+}
+
+static void bissellBufferKinestate(void)
+{
+	ksU32 deltaAbs = 0x00;
+	deltaAbs = thorGetAbsVal(lv_bl_resistorValPrev, lv_bl_resistorVal);
+
+#if   (__ANTI_NOISE_STRATEGY >= __ANTI_NOISE_LEVEL_4)
+	deltaAbs >>= 0x03;
+#elif (__ANTI_NOISE_STRATEGY >= __ANTI_NOISE_LEVEL_3)
+  deltaAbs >>= 0x02;
+#elif (__ANTI_NOISE_STRATEGY >= __ANTI_NOISE_LEVEL_2)
+  deltaAbs >>= 0x01;
+#endif
+
+#if (__ANTI_NOISE_STRATEGY != __ANTI_NOISE_LEVEL_0)
+  deltaAbs -= !!deltaAbs;
+#else
+
+#endif
+
+	if (0x05 == lv_BL_qActiveUsed)
+	{ // the Q is full
+		lv_BL_KineState -= lv_BL_qActiveBuf[lv_BL_qActiveHead];
+		++lv_BL_qActiveHead;
+		lv_BL_qActiveHead %= 0x05; // pop Data from Head
+		--lv_BL_qActiveUsed;
+	}
+
+	lv_BL_qActiveBuf[lv_BL_qActiveTail] = deltaAbs;     // put Data at Tail
+	++lv_BL_qActiveTail;
+	lv_BL_qActiveTail %= 0x05;
+	++lv_BL_qActiveUsed;
+
+	lv_BL_KineState += deltaAbs; // less than 255
 }
 
 /**
@@ -400,13 +446,16 @@ static void thorBufferKinestate (void)
  */
 int main()
 {
-  mcuBSPInit(aiDeBaoReturnRecvISR);
+	unsigned int k1 = 0, k2 = 0;
+	mcuBSPInit(aiDeBaoReturnRecvISR);
 
-  uTracer[__UTRACER_IDX_1].pUsart->gpio[_KS_USART_IO_TXD].Pin.pinNum = (ksGPIO_PIN_e)__PIN_TO_PC;
-  ksGpioInit(&uTracer[__UTRACER_IDX_1].pUsart->gpio[_KS_USART_IO_TXD]);
+	uTracer[__UTRACER_IDX_1].pUsart->gpio[_KS_USART_IO_TXD].Pin.pinNum =
+			(ksGPIO_PIN_e) __PIN_TO_PC;
+	ksGpioInit(&uTracer[__UTRACER_IDX_1].pUsart->gpio[_KS_USART_IO_TXD]);
 
-  uTracer[__UTRACER_IDX_1].pUsart->gpio[_KS_USART_IO_RXD].Pin.pinNum = (ksGPIO_PIN_e)__PIN_FROM_PC;
-  ksGpioInit(&uTracer[__UTRACER_IDX_1].pUsart->gpio[_KS_USART_IO_RXD]);
+	uTracer[__UTRACER_IDX_1].pUsart->gpio[_KS_USART_IO_RXD].Pin.pinNum =
+			(ksGPIO_PIN_e) __PIN_FROM_PC;
+	ksGpioInit(&uTracer[__UTRACER_IDX_1].pUsart->gpio[_KS_USART_IO_RXD]);
 
 #if (__THOR_MODE == _THOR_WANMO)
   uTracer[__UTRACER_IDX_1].pUsart->gpio[_KS_USART_IO_RXD].Pin.pinNum = (ksGPIO_PIN_e)__PIN_FROM_WM;
@@ -414,47 +463,79 @@ int main()
 #endif
 
 #if (__THOR_MODE == _THOR_NORMAL)
-  tm30AdcTestInit(&gv_tm30TestAdc,
-                  _ISR_ADC_DONE, // _ISR_ADC_BUFFER_FUL,
-                  tm30AdcTestCallBack);
+	tm30AdcTestInit(&gv_tm30TestAdc,
+	_ISR_ADC_DONE, // _ISR_ADC_BUFFER_FUL,
+			tm30AdcTestCallBack);
 #endif
 
-  uTracerRemapUart(__PIN_TO_PC);
-  __uTRACER_PRINTF(__TRACER_OUT, true, "\n*********************************************\n");
-  __uTRACER_PRINTF(__TRACER_OUT, true, "***********The thor is running***************\n");
-  __uTRACER_PRINTF(__TRACER_OUT, true, "*********************************************\n");
+	uTracerRemapUart(__PIN_TO_PC);
+	__uTRACER_PRINTF(__TRACER_OUT, true,
+			"\n*********************************************\n");
+	__uTRACER_PRINTF(__TRACER_OUT, true,
+			"***********The thor is running***************\n");
+	__uTRACER_PRINTF(__TRACER_OUT, true,
+			"*********************************************\n");
 
-  lv_filteredADC = 0x00;
-  thorDeInitKineStateQ();
-  while(true)
-  {
-    uTracerRemapUart(__PIN_TO_ADB);
-    do
-    { // receive forve value from aidebao
-      __uTRACER_SEND_BUF(__TRACER_OUT,  &lc_cmdGetForce, sizeof(lc_cmdGetForce));
-      ksDelayMsByLoop(200);
-    } while(true && (_tracer_Recved != uTracer[__UTRACER_IDX_1].rxBuf.status));
+	lv_filteredADC = 0x00;
+	lv_bl_filteredADC = 0x00;
+	thorDeInitKineStateQ();
+	while (true)
+	{
+		/*
+		 uTracerRemapUart(__PIN_TO_ADB);
+		 do
+		 { // receive forve value from aidebao
+		 __uTRACER_SEND_BUF(__TRACER_OUT,  &lc_cmdGetForce, sizeof(lc_cmdGetForce));
+		 ksDelayMsByLoop(200);
+		 } while(true && (_tracer_Recved != uTracer[__UTRACER_IDX_1].rxBuf.status));
 
-    ksU32 *pForceInteger = (ksU32 *)&uTracer[__UTRACER_IDX_1].rxBuf.buf[0x03];
-    *pForceInteger = ks_htonl(*pForceInteger);
+		 ksU32 *pForceInteger =
+		 (ksU32*) &uTracer[__UTRACER_IDX_1].rxBuf.buf[0x03];
+		 *pForceInteger = ks_htonl(*pForceInteger);
 
-    float *pForceFloat = (float *)&uTracer[__UTRACER_IDX_1].rxBuf.buf[0x03];
+		 float *pForceFloat = (float*) &uTracer[__UTRACER_IDX_1].rxBuf.buf[0x03];
 
-    lv_forceInGF   = *pForceFloat;
-    lv_forceInGF  *= -1000.0f;
+		 lv_forceInGF = *pForceFloat;
+		 lv_forceInGF *= -1000.0f;
 
-    lv_aiDeBaoCntr = 0x00;
-    deInitUtracerRxBuf();
+		 lv_aiDeBaoCntr = 0x00;
+		 deInitUtracerRxBuf();
+		 //*/
 
 #if   (__THOR_MODE == _THOR_NORMAL)
-    tm30AdcDoSampleing(&gv_tm30TestAdc);
 
-    // calculation
-    lv_filteredADC      = rcFilter(gv_tm30TestAdc.adcBuf[0], lv_filteredADC, 5);
-    lv_Vout             = lv_filteredADC * __REF_VOLTAGE / 4096;
+		tm30AdcDoSampleing(&gv_tm30TestAdc);
 
-    lv_resistorValPrev  = lv_resistorVal;
-    lv_resistorVal      = (__SUPPLY_VOLTAGE -  lv_Vout) * __DIVIDOR_RESISTOR /  lv_Vout;
+		/*
+		 __uTRACER_PRINTF(__TRACER_OUT, true, "%d, %d, %d, %d, %d, %d, %d, %d\n",
+		 gv_tm30TestAdc.adcBuf[0],
+		 gv_tm30TestAdc.adcBuf[1],
+		 gv_tm30TestAdc.adcBuf[2],
+		 gv_tm30TestAdc.adcBuf[3],
+		 gv_tm30TestAdc.adcBuf[4],
+		 gv_tm30TestAdc.adcBuf[5],
+		 gv_tm30TestAdc.adcBuf[6],
+		 gv_tm30TestAdc.adcBuf[7]);
+
+		 //*/
+
+		// calculation
+		lv_filteredADC = rcFilter(gv_tm30TestAdc.adcBuf[0], lv_filteredADC, 5);
+		lv_Vout = lv_filteredADC * __REF_VOLTAGE / 4096;
+
+		lv_resistorValPrev = lv_resistorVal;
+		lv_resistorVal = (__SUPPLY_VOLTAGE - lv_Vout) * __DIVIDOR_RESISTOR
+				/ lv_Vout;
+
+		tm30AdcDoSampleing(&gv_tm30TestAdc);
+
+		lv_bl_filteredADC = rcFilter(gv_tm30TestAdc.adcBuf[0],
+				lv_bl_filteredADC, 5);
+		lv_bl_Vout = lv_bl_filteredADC * __REF_VOLTAGE / 4096;
+
+		lv_bl_resistorValPrev = lv_bl_resistorVal;
+		lv_bl_resistorVal = (__SUPPLY_VOLTAGE - lv_bl_Vout) * __DIVIDOR_RESISTOR
+				/ lv_bl_Vout;
 
 #elif (__THOR_MODE == _THOR_WANMO)
     deInitUtracerRxBuf();
@@ -480,35 +561,74 @@ int main()
     uTracerRemapUart(__PIN_TO_ADB);
 #endif
 
-    thorBufferKinestate();
+		thorBufferKinestate();
+		bissellBufferKinestate();
 
-    if (lv_resistorVal == 0)
-    {
-      lv_resistorVal = 1;
-    }
+		if (lv_resistorVal == 0)
+		{
+			lv_resistorVal = 1;
+		}
+		if (lv_bl_resistorVal == 0)
+		{
+			lv_bl_resistorVal = 1;
+		}
 
-    ksS32 gfInt     = (ksS32)(lv_forceInGF * 10);
-    ksS32 gfDecimal = gfInt % 10;
-    if (gfDecimal < 0)
-    {
-      gfDecimal = -1 * gfDecimal;
-    }
+		ksS32 gfInt = (ksS32) (lv_forceInGF * 10);
+		ksS32 gfDecimal = gfInt % 10;
+		if (gfDecimal < 0)
+		{
+			gfDecimal = -1 * gfDecimal;
+		}
 
-    uTracerRemapUart(__PIN_TO_PC);
-    __uTRACER_PRINTF(__TRACER_OUT,
-                     true,
-                     "%1d : %4d.%1d, %7d, %4d, %3d\n",
-                     1,
-                     gfInt / 10l,      // force gauge
-                     gfDecimal,        // force gauge
-                     lv_resistorVal,   // resisor
-                     lv_Vout,          // Vout
-                     lv_THOR_KineState * 100 / lv_resistorVal); // in 100%
+		k1 = lv_THOR_KineState * 100 / lv_resistorVal;
+		k2 = lv_BL_KineState * 100 / lv_bl_resistorVal;
+		uTracerRemapUart(__PIN_TO_PC);
+		__uTRACER_PRINTF(__TRACER_OUT, true, "%7d, %4d, %3d, %7d, %4d, %3d\n",
+				lv_resistorVal, // resisor
+				lv_Vout,// Vout
+				k1,// in 100%
+				lv_bl_resistorVal,// resisor
+				lv_bl_Vout,// Vout
+				k2);// in 100%
 
-    ksDelayMsByLoop(60);
-  }
+		//*/
+		if (lv_Vout > 300 || lv_bl_Vout > 300)
+		{
+			LEDs_On(&gv_ledPeriph[0]); //马达启动
+			if (lv_Vout > lv_bl_Vout)
+			{
+				if (lv_Vout > 300 && k1 <= 1 && (lv_Vout - lv_bl_Vout) > 100)
+				{
+					LEDs_On(&gv_ledPeriph[1]);
+					LEDs_Off(&gv_ledPeriph[6]);
+				}
+				else
+				{
+					LEDs_Off(&gv_ledPeriph[1]);
+				}
+			}
+			else
+			{
+				if (lv_bl_Vout > 300 && k2 <= 1 && (lv_bl_Vout - lv_Vout) > 100)
+				{
+					LEDs_On(&gv_ledPeriph[6]);
+					LEDs_Off(&gv_ledPeriph[1]);
+				}
+				else
+				{
+					LEDs_Off(&gv_ledPeriph[6]);
+				}
+			}
+		}
+		else
+		{
+			LEDs_Off(&gv_ledPeriph[0]);
+			LEDs_Off(&gv_ledPeriph[1]);
+			LEDs_Off(&gv_ledPeriph[6]);
+		}
+		ksDelayMsByLoop(100);
+	}
 }
-
 
 #ifdef  __cplusplus
 //*****************************************************************************
